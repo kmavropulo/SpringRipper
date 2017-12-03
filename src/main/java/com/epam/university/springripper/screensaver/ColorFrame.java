3057e32a4fe291f4e4f821446396e0ebbac6e238
Konstantin Mavropulo <konstantin.n.mavropulo@gmail.com>
2017-12-03 21:35:35 +0300

    Dev: new package with ColorFrame("singleton" extending JFrame 200*200, that is repainting and instantiating in new place of monitor every time with a new color) class and Config class (with Color bean, "prototype", ScopedProxyMode.TARGET_CLASS parameter in @Scope annotation is the solution to do the color and prototype the real random ("prototype")) have been added. AnnotationConfigApplicationContext is using to get the bean.

---
+++ src/main/java/com/epam/university/springripper/screensaver/ColorFrame.java
@@ -0,0 +1,31 @@
+package com.epam.university.springripper.screensaver;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+@Component
+//default spring's scope is @Scope("singleton")
+public class ColorFrame extends JFrame {
+    @Autowired
+    private Color color;
+
+    public ColorFrame() {
+        setSize(200, 200);
+        setVisible(true);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    public void showOnRandomPlace() {
+        Random random = new Random();
+        setLocation(random.nextInt(1720), random.nextInt(1880));
+        getContentPane().setBackground(color);
+        repaint();
+    }
+
+}
---
+++ src/main/java/com/epam/university/springripper/screensaver/Config.java
@@ -0,0 +1,34 @@
+package com.epam.university.springripper.screensaver;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+
+import java.awt.Color;
+import java.util.Random;
+
+@Configuration
+@ComponentScan(basePackages = "com.epam.university.springripper.screensaver")
+public class Config {
+    @Bean
+    @Scope(value = "prototype",proxyMode = ScopedProxyMode.TARGET_CLASS)
+    public Color color() {
+        Random random = new Random();
+        return new Color(random.nextInt(255), random.nextInt(255), random.nextInt(
+                255));
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext(Config.class);
+
+        while (true) {
+            annotationConfigApplicationContext.getBean(ColorFrame.class).showOnRandomPlace();
+            Thread.sleep(50);
+        }
+    }
+}

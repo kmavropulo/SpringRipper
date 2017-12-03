ab35df5ab11be194c37af32338e17e47e2c933ce
Konstantin Mavropulo <konstantin.n.mavropulo@gmail.com>
2017-12-03 12:13:12 +0300

    Dev: InjectRandomIntAnnotationBeanPostProcessor and InjectRandomInt annotation added.

--- src/main/java/com/epam/university/springripper/quoters/TerminatorQuoter.java
+++ src/main/java/com/epam/university/springripper/quoters/TerminatorQuoter.java
@@ -3,14 +3,22 @@ package com.epam.university.springripper.quoters;
 import lombok.Setter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 @Setter
 public class TerminatorQuoter implements Quoter {
     private static final Logger logger = LoggerFactory.getLogger(TerminatorQuoter.class);
+    @InjectRandomInt(min = 2, max = 7)
+    private int repeat;
     private String message;
+
 
     @Override
     public void sayQuote() {
-        logger.debug(message);
+        System.out.println("test"+ repeat);
+
+        for (int i = 0; i < repeat; i++) {
+            logger.debug("message" + message);
+        }
     }
 }
